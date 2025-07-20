6800f0e1b98e6f5965e91acaa2e2fa69654c9f27
whdzera <whdzera@gmail.com>
2025-07-20 17:38:04 +0700

    added theme controller stimulus for dark light mode

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,5 +2,7 @@ import { Application } from "@hotwired/stimulus";
 
 window.Stimulus = Application.start();
 
+import ThemeController from "./controllers/theme_controller.js";
+Stimulus.register("theme", ThemeController);
 import CalcController from "./controllers/calc_controller.js";
 Stimulus.register("calculator", CalcController);
